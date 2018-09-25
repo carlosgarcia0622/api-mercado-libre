acff244a316566b86cfa90f1b7aede47fd14dfed
Carlos Garcia <carlos.garcia3@udea.edu.co>
2018-09-25 18:08:16 -0500

    Form

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
 <body>
+
+<form action="" method="get">
+  <label for="Buscar"></label>
+  <input type="text" name="txtbuscar">
+  <button tupe="submit" name="btnbuscar">Buscar</button>
+</form>
+
 <?php echo '<p>Hola Mundo '.$_GET["nombre"].'</p>';?>
 <?php echo '<p>Hola Mundo 2 '.$_GET["apellido"].'</p>';?>
 <div id="app">
