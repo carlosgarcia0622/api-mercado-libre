492e7d5d6bf55f5e2209a64e1f813df37812dc1e
Carlos Garcia <carlos.garcia3@udea.edu.co>
2018-09-25 17:58:29 -0500

    solucion

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <body>
 <?php echo '<p>Hola Mundo '.$_GET["nombre"].'</p>';?>
 <?php echo '<p>Hola Mundo 2 '.$_GET["apellido"].'</p>';?>
-<?php echo $ curl -X GET 'https://api.mercadolibre.com/users/226384143/items/search?sku=abcd1234&status=active&access_token=$ACCESS_TOKEN';?>
 <div id="app">
     {{ message }}
   </div>
