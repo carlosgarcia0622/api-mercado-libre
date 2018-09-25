fc399a040ad2e7551add71585bacb7a8de23215f
Carlos Garcia <carlos.garcia3@udea.edu.co>
2018-09-25 11:03:09 -0500

    include vue.js

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <html>
 <head>
-<title>Prueba de carlosgarcia0622</title>
+<title>Api mercado libre para busqueda de productos</title>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
 <body>
 <?php echo '<p>Hola Mundo '.$_GET["nombre"].'</p>';?>
 <?php echo '<p>Hola Mundo 2 '.$_GET["apellido"].'</p>';?>
+<div id="app">
+    {{ message }}
+  </div>
+
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {
+        message: 'Hello Vue!'
+      }
+    })
+  </script>
 </body>
 </html>
